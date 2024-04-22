0f1aa06928975a9f15aeeb72cbb50e1482a2a196
MorganeGF <morgane.guerrero@live.be>
2024-04-22 16:46:19 +0200

    comment dev condition for url

--- src/lib/js/getMyUrlForDev.js
+++ src/lib/js/getMyUrlForDev.js
@@ -2,9 +2,9 @@ import { dev } from '$app/environment';
 
 let getMyUrlForDev = () => {
 	let urlDev = 'https://tfe30-backend.up.railway.app';
-	if (dev) {
-		urlDev = import.meta.env.VITE_BACKEND_URL;
-	}
+	// if (dev) {
+	// 	urlDev = import.meta.env.VITE_BACKEND_URL;
+	// }
 	return urlDev;
 };
 
