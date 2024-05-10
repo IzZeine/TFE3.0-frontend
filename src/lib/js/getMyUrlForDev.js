afb35877c9ad50c9736ea02ad44dbf35d8cd5425
MorganeGF <morgane.guerrero@live.be>
2024-05-10 15:15:10 +0200

    change url io

--- src/lib/js/getMyUrlForDev.js
+++ src/lib/js/getMyUrlForDev.js
@@ -1,7 +1,7 @@
 let getMyUrlForDev = () => {
-	// let urlDev = 'https://tfe30-backend.up.railway.app';
+	let urlDev = 'https://tfe30-backend.up.railway.app';
 	// let urlDev = 'http://localhost:3000';
-	let urlDev = 'http://192.168.0.43:3000';
+	// let urlDev = 'http://192.168.0.43:3000';
 
 	return urlDev;
 };
--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -3,9 +3,9 @@ import { io } from 'socket.io-client';
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load() {
-	// let socket = io('https://tfe30-backend.up.railway.app');
+	let socket = io('https://tfe30-backend.up.railway.app');
 	// let socket = io('http://localhost:3000');
-	let socket = io('http://192.168.0.43:3000');
+	// let socket = io('http://192.168.0.43:3000');
 
 	return {
 		socket
