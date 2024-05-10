b391e0fa54223404f1967f0dbbb2da0b657f3550
MorganeGF <morgane.guerrero@live.be>
2024-05-10 15:25:58 +0200

    change url IO

--- src/lib/js/socketConnection.js
+++ src/lib/js/socketConnection.js
@@ -1,8 +1,8 @@
 import ioClient from 'socket.io-client';
 
 // const ENDPOINT = 'http://192.168.0.43:3000';
-const ENDPOINT = 'https://tfe30-backend.up.railway.app';
-// const ENDPOINT = 'http://localhost:3000';
+// const ENDPOINT = 'https://tfe30-backend.up.railway.app';
+const ENDPOINT = 'http://localhost:3000';
 
 const io = ioClient(ENDPOINT);
 
