a7c2da2df3a3088c398442a5a049d3c32ffa5b8e
Julien Moreau <julien@thylo.be>
2024-05-14 17:05:22 +0200

    dockerfile

--- src/lib/js/getMyUrlForDev.js
+++ src/lib/js/getMyUrlForDev.js
@@ -3,6 +3,7 @@ let getMyUrlForDev = () => {
 	// let urlDev = 'http://192.168.0.43:3000';
 
 	//return 'http://localhost:3000';
+	console.log('PUBLIC_BACKEND_URL', import.meta.env.VITE_BACKEND_URL);
 	return import.meta.env.VITE_BACKEND_URL;
 };
 
