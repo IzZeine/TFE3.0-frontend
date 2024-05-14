03a2ef676991f0f96a1fa0b85919316eec202eb7
Julien Moreau <julien@thylo.be>
2024-05-14 17:08:24 +0200

    dockerfile

--- src/lib/js/getMyUrlForDev.js
+++ src/lib/js/getMyUrlForDev.js
@@ -1,10 +1,12 @@
+import { PUBLIC_BASE_URL } from '$env/static/public';
+
 let getMyUrlForDev = () => {
 	// let urlDev = 'https://tfe30-backend.up.railway.app';
 	// let urlDev = 'http://192.168.0.43:3000';
 
 	//return 'http://localhost:3000';
-	console.log('PUBLIC_BACKEND_URL', import.meta.env.VITE_BACKEND_URL);
-	return import.meta.env.VITE_BACKEND_URL;
+	console.log('PUBLIC_BACKEND_URL', PUBLIC_BASE_URL);
+	return import.meta.env.PUBLIC_BASE_URL;
 };
 
 export { getMyUrlForDev };
