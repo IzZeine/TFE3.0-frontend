e62daa116cbacb8e475f991e3aa9769688ea191d
MorganeGF <morgane.guerrero@live.be>
2024-05-25 20:03:04 +0200

    add blur  animation

--- src/lib/game/Map/Map.svelte
+++ src/lib/game/Map/Map.svelte
@@ -2,8 +2,9 @@
 	import GameArrows from '$lib/game/Map/GameArrows.svelte';
 	import GameHeader from '$lib/game/Map/GameHeader.svelte';
 	import GameFooter from '$lib/game/Map/GameFooter.svelte';
-	import { user } from '$lib/api/stores';
+	import { fade, fly, blur, slide } from 'svelte/transition';
 	import GameDialogs from '$lib/game/Map/dialogs/GameDialogs.svelte';
+	import { user } from '$lib/api/stores';
 
 	export let items, game;
 
@@ -14,12 +15,14 @@
 	}
 </script>
 
-<div class="maxContent">
-	<div class="mapUserContainer">
-		<GameHeader />
-		<GameArrows />
-		<GameFooter on:openDialog={setDialog} />
+{#key user}
+	<div class="maxContent" in:blur={{ duration: 300 }} out:blur={{ duration: 300 }}>
+		<div class="mapUserContainer">
+			<GameHeader />
+			<GameArrows />
+			<GameFooter on:openDialog={setDialog} />
+		</div>
 	</div>
-</div>
+{/key}
 
 <GameDialogs {currentDialog} {items} {game} on:closeDialog={() => setDialog('')} />
--- src/lib/game/Map/dialogs/GameDialogs.svelte
+++ src/lib/game/Map/dialogs/GameDialogs.svelte
@@ -2,6 +2,7 @@
 	import { createEventDispatcher } from 'svelte';
 	import PowerDialogs from './PowerDialogs.svelte';
 	import Inventory from './Inventory.svelte';
+	import { fade, fly, blur, slide } from 'svelte/transition';
 	import Item from './Item.svelte';
 
 	export let items, currentDialog, game;
@@ -15,12 +16,16 @@
 	}
 </script>
 
-{#if currentDialog === 'power'}
-	<PowerDialogs {currentDialog} {game} on:closeDialog={closeDialog} />
-{/if}
-{#if currentDialog === 'inventory'}
-	<Inventory {items} {currentDialog} on:closeDialog={closeDialog} />
-{/if}
-{#if currentDialog === 'item'}
-	<Item {items} {currentDialog} on:closeDialog={closeDialog} />
+{#if currentDialog}
+	<div class="dialog" in:blur={{ duration: 300 }} out:blur={{ duration: 300 }}>
+		{#if currentDialog === 'power'}
+			<PowerDialogs {currentDialog} {game} on:closeDialog={closeDialog} />
+		{/if}
+		{#if currentDialog === 'inventory'}
+			<Inventory {items} {currentDialog} on:closeDialog={closeDialog} />
+		{/if}
+		{#if currentDialog === 'item'}
+			<Item {items} {currentDialog} on:closeDialog={closeDialog} />
+		{/if}
+	</div>
 {/if}
